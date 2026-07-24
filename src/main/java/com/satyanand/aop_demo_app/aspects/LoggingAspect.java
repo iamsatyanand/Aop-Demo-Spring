95bcf2fc82af72b594412c4238cd7e365cd7398f
iamsatyanand <iamsatyanandmishra@gmail.com>
2026-07-24 10:20:40 +0530

    logging aspects implementation with few advises

---
+++ src/main/java/com/satyanand/aop_demo_app/aspects/LoggingAspect.java
@@ -0,0 +1,37 @@
+package com.satyanand.aop_demo_app.aspects;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+//    // * - any kind of return type
+//    // before any method call named as orderPackage in any class
+//    // (..) method accepting any number of arguments
+//    @Before("execution (* orderPackage(..))")
+//    // inside the impl folder orderPackage present in any number of files
+//    @Before("execution(* com.satyanand.aop_demo_app.services.impl.*.orderPackage(..))")
+//    // before orderPackage inside this particular class lie in this path only
+//    @Before("execution(* com.satyanand.aop_demo_app.services.impl.ShipmentServiceImpl.orderPackage(..))")
+    // call this aspect before any method any class inside impl package
+    @Before("execution(* com.satyanand.aop_demo_app.services.impl.*.*(..))")
+    public void beforeOrderPackage(JoinPoint joinPoint){
+        log.info("Before aspect called from Logging aspect kind, {}",joinPoint.getKind() );
+        log.info("Before aspect called from Logging aspect signature, {}", joinPoint.getSignature());
+    }
+
+//    // call within any package not only for single method but for every files inside any package
+//    @Before("within (com.satyanand.aop_demo_app.services.impl.*)")
+    // call all the class and methods inside any package or the subpackages within aop_demo_app package
+    @Before("within (com.satyanand.aop_demo_app..*)")
+    public void beforeServiceImplCalls(){
+        log.info("Service impl calls");
+    }
+
+}
--- src/test/java/com/satyanand/aop_demo_app/services/impl/ShipmentServiceImplTest.java
+++ src/test/java/com/satyanand/aop_demo_app/services/impl/ShipmentServiceImplTest.java
@@ -2,12 +2,14 @@ package com.satyanand.aop_demo_app.services.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 @Slf4j
 public class ShipmentServiceImplTest {
 
+    @Autowired
     private ShipmentServiceImpl service;
 
     @Test
